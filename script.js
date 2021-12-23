1957902881bffe8dd8273bf0853757b12755ef00
S Roze <sherozem4@gmail.com>
2021-12-23 11:20:29 +0000

    To start the game with a blank slate, the new game button has had functionality added to it to allow the user to begin with 0 score, and the dice image also is hidden from the screen.

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 'use strict';
 // Starter file comment to initialise repository changes -- Test1
+
+// Starter
+
+const score = document.querySelectorAll('.score');
+const currentScore = document.querySelectorAll('.current-score');
+const dice = document.querySelector('.dice');
+const maxPoints = 100;
+
+document.querySelector('.btn--new').addEventListener('click', function () {
+  // Reset the score to -0
+  for (let i = 0; i < score.length; i++) {
+    score[i].textContent = 0;
+    currentScore[i].textContent = 0;
+    console.log('This button is being pressed');
+  }
+
+dice.style.display = 'none';
+});
+
+if(score != maxPoints) { 
+    // then perform the entire code
+} else {
+    // type the message for who the winner is.
+ }
+
