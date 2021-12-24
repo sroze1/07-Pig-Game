c91f3aa840f922f41138ed86edf8288637c9a402
S Roze <sherozem4@gmail.com>
2021-12-24 19:37:29 +0000

    final addition. Thanks

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
